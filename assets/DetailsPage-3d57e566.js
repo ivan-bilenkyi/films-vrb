00de44ce089758c5fdcabc9bd8321d5199c055d6
ivan-bilenkyi <128532966+ivan-bilenkyi@users.noreply.github.com>
2024-04-13 17:53:34 +0000

    Deploying to gh-pages from @ ivan-bilenkyi/films-vrb@8b76042d6510424d76011c5ed047ad25ee49abb0 🚀

---
+++ assets/DetailsPage-3d57e566.js
@@ -0,0 +1,11 @@
+import{s as a,L as n,u as r,r as i,j as e}from"./index-54efbbd3.js";import{G as l}from"./iconBase-297e52da.js";const x=a(n)`
+text-decoration: none;
+display: block;
+margin: 40px 0;
+text-align: center;
+color: #22389e;
+
+&:hover {
+    color: #c92d2d;
+}
+`;function d(t){return l({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z"},child:[]}]})(t)}function u(){var o;const t=r(),c=i.useRef(t),s=t.state.item;return console.log(s),e.jsxs(x,{to:((o=c.current.state)==null?void 0:o.from)??"/",children:[e.jsx(d,{style:{width:"16px",height:"16px",verticalAlign:"bottom"}})," Go back"]})}export{u as default};
--- assets/FavoritePage-ec3f55e7.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-e97e57d5.js";function e(){return r.jsx("h1",{children:"favorite"})}export{e as default};
