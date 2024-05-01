9ca98d964ae8b574004cfc3279de35dddffbcc07
ivan-bilenkyi <128532966+ivan-bilenkyi@users.noreply.github.com>
2024-05-01 20:27:01 +0000

    Deploying to gh-pages from @ ivan-bilenkyi/films-vrb@2f21005336b249d61d21eba92f07c9dd3324c887 🚀

--- assets/DetailsPage.styled-95b1c173.js
+++ assets/DetailsPage.styled-95b1c173.js
@@ -1,4 +1,4 @@
-import{s as t,G as i,L as a}from"./index-3ccd622f.js";const e=t.div`
+import{s as t,G as i,L as a}from"./index-1cde8d4e.js";const e=t.div`
     position: absolute;
     top: 0;
     left: 0;
