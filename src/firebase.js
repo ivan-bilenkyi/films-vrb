b338413d3b9b236fb8ac395dc2d5a039ddfdc0ac
ivanbilenkyi <ivanbilenky@gmail.com>
2024-05-11 16:43:46 +0300

    fix databaseURL

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getDatabase } from 'firebase/database';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
-const DATABASE_URI = import.meta.env.VITE_DATABASE_URI;
+const DATABASE_URL = import.meta.env.VITE_DATABASE_URL;
 const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
 const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
 const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
@@ -14,7 +14,7 @@ const APP_ID = import.meta.env.VITE_APP_ID;
 const firebaseConfig = {
     apiKey: API_KEY,
     authDomain: AUTH_DOMAIN,
-    databaseURL: 'https://films-59b2a-default-rtdb.europe-west1.firebasedatabase.app',
+    databaseURL: DATABASE_URL,
     projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
     messagingSenderId: MESSAGING_SENDER_ID,
